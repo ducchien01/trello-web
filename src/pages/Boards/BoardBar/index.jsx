2a2c970240033c577f5ecdb0cae3463f6b0f462a
ducchien01 <chien23964@nuce.edu.vn>
2025-07-18 03:10:18 +0700

    board-bar

--- src/pages/Boards/BoardBar/index.jsx
+++ src/pages/Boards/BoardBar/index.jsx
@@ -1,15 +1,103 @@
 import Box from '@mui/material/Box'
+import Chip from '@mui/material/Chip'
+import DashboardIcon from '@mui/icons-material/Dashboard'
+import VpnLockIcon from '@mui/icons-material/VpnLock'
+import AddToDriveIcon from '@mui/icons-material/AddToDrive'
+import BoltIcon from '@mui/icons-material/Bolt'
+import FilterListIcon from '@mui/icons-material/FilterList'
+import Avatar from '@mui/material/Avatar'
+import AvatarGroup from '@mui/material/AvatarGroup'
+import Tooltip from '@mui/material/Tooltip'
+import Button from '@mui/material/Button'
+import PersonAddIcon from '@mui/icons-material/PersonAdd'
+
+const MENU_STYLE = {
+  color: 'primary.main',
+  bgcolor: 'white',
+  border: 'none',
+  paddingX: '5px',
+  borderRadius: '4px',
+  '& .MuiSvgIcon-root': {
+    color: 'primary.main'
+  },
+  '&:hover': {
+    bgcolor: 'primary.50'
+  }
+}
 
 function BoardBar() {
   return (
-    <Box sx={{
-      backgroundColor: 'primary.dark',
+    <Box px={2} sx={{
+
       width: ' 100%',
       height: (theme) => theme.trello.boardBarHeight,
       display: 'flex',
-      alignItems: 'center'
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      gap: 1,
+      overflowX: 'auto',
+      borderTop: '1px solid #00bfa5'
     }}>
-      Board bar
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+        <Chip
+          icon={<DashboardIcon />}
+          label="Ducchien01"
+          // clickable
+          onClick={() => { }}
+          sx={MENU_STYLE}
+        />
+        <Chip
+          icon={<VpnLockIcon />}
+          label="Public/Private Workplace"
+          onClick={() => { }}
+          sx={MENU_STYLE}
+        />
+        <Chip
+          icon={<AddToDriveIcon />}
+          label="Add to Google Drive"
+          onClick={() => { }}
+          sx={MENU_STYLE}
+        />
+        <Chip
+          icon={<BoltIcon />}
+          label="Automation"
+          onClick={() => { }}
+          sx={MENU_STYLE}
+        />
+        <Chip
+          icon={<FilterListIcon />}
+          label="Filter"
+          onClick={() => { }}
+          sx={MENU_STYLE}
+        />
+      </Box>
+
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+        <Button variant="outlined" startIcon={<PersonAddIcon />}>Invite</Button>
+        <AvatarGroup
+          max={3}
+          sx={{
+            '& .MuiAvatar-root': {
+              width: 34,
+              height: 34,
+              fontSize: 16
+            }
+          }}
+        >
+          <Tooltip title="ducchien">
+            <Avatar alt="Trevor Henderson" src="/static/images/avatar/5.jpg" />
+          </Tooltip>
+          <Tooltip title="ducchien">
+            <Avatar alt="Trevor Henderson" src="/static/images/avatar/5.jpg" />
+          </Tooltip>
+          <Tooltip title="ducchien">
+            <Avatar alt="Trevor Henderson" src="/static/images/avatar/5.jpg" />
+          </Tooltip>
+          <Tooltip title="ducchien">
+            <Avatar alt="Trevor Henderson" src="/static/images/avatar/5.jpg" />
+          </Tooltip>
+        </AvatarGroup>
+      </Box>
     </Box>
   )
 }
